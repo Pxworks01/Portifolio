f5cde49ecec6280468ab1ea09d8e2c933124269a
Pxworks01 <pedro.paxola301@gmail.com>
2025-06-24 21:01:28 -0300

    :tada: Commit inicial

---
+++ JS/java.js
@@ -0,0 +1,12 @@
+function toggleMode(){
+    const html = document.documentElement;
+    html.classList.toggle("light");
+
+    const img = document.querySelector("#profile img");
+
+    if(html.classList.contains("light")){
+        img.setAttribute("src","../IMG/perfil2.png");
+    }else {
+        img.setAttribute("scr","../IMG/perfil2.png");
+    }
+}
